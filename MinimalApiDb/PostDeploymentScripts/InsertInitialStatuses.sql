a96782b09ed4bb78dab7bd12a98226290717a239
DESKTOP-HGRP1DQ\leon <szymonwojcik800@gmail.com>
2023-08-11 17:03:11 +0200

    Add initial data seeding

---
+++ MinimalApiDb/PostDeploymentScripts/InsertInitialStatuses.sql
@@ -0,0 +1,15 @@
+﻿BEGIN
+
+DECLARE @statusesCount int;
+
+SELECT @statusesCount = COUNT(*) FROM TodoStatuses;
+
+--Create initial statuses if statuses table is empty
+IF(@statusesCount = 0) 
+	INSERT INTO TodoStatuses (Name, Description)
+	VALUES (N'New', N'Newly created todo'),
+	(N'In progress', N'Todo in progress'),
+	(N'Completed', N'Completed todo');
+
+
+END;
